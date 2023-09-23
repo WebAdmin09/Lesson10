93bc84e4d4045cad5b669e179aa0f10766d5219d
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-23 13:10:31 +0500

    dxsdvsv

--- src/pages/TeacherPage.jsx
+++ src/pages/TeacherPage.jsx
@@ -1,6 +1,7 @@
-import { Space, Table, Button, Modal, Form, Input, Checkbox, } from 'antd';
+import { Space, Table, Button, Modal, Form, Input, Checkbox } from 'antd';
 import { useState, useEffect, Fragment } from 'react';
 import request from '../Api';
+
 const TeacherPage = () => {
   const columns = [
     {
@@ -31,13 +32,6 @@ const TeacherPage = () => {
     {
       title: 'Student',
       key: 'action',
-      render: (_, record) => {
-        return (
-          <Space size="middle">
-            <Button>Students</Button>
-          </Space>
-        )
-      },
     },
     {
       title: 'Action',
@@ -45,7 +39,7 @@ const TeacherPage = () => {
       render: (_, record) => {
         return (
           <Space size="middle">
-            <Button type='primary'>Edit</Button>
+            <Button type='primary' onClick={() => edit(record.id)}>Edit</Button>
             <Button danger type='primary'>Delete</Button>
           </Space>
         )
@@ -55,115 +49,136 @@ const TeacherPage = () => {
   const [form] = Form.useForm();
   const [data, setData] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selected, setSelected] = useState(null)
+  
   useEffect(() => {
     getData()
-  }, [])
+  }, []);
+
   async function getData() {
     try {
-      let { data } = await request.get('teacher')
-      setData(data)
+      let { data } = await request.get('teacher');
+      setData(data);
     } catch (error) {
       console.log(error);
     }
   }
 
   const showModal = () => {
+    setSelected(null)
     setIsModalOpen(true);
+    form.resetFields();
   };
 
   const handleOk = async () => {
     try {
-      const values = await form.validateFields();
-      await request.post('teacher', values)
+      await form.validateFields();
+      const values = form.getFieldsValue();
+      if(selected === null ){
+        await request.post('teacher', values);
+      }else{
+        await request.put(`teacher/${selected}`, values);
+      }
+      getData();
       setIsModalOpen(false);
     } catch (error) {
       console.log(error);
     }
-
   };
 
-const handleCancel = () => {
-  setIsModalOpen(false);
-};
-return <Fragment>
-  <Table bordered
-    title={() => <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <h1>Teachers</h1>
-      <Button onClick={showModal} type='primary'>Add</Button>
-    </div>}
-    columns={columns} dataSource={data} />
-  <Modal
-    title="Teacher Modal"
-    open={isModalOpen}
-    onOk={handleOk}
-    onCancel={handleCancel}
-    okText='Add Teacher'
-  >
-    <Form
-      form={form}
-      name="basic"
-      labelCol={{
-        span: 24,
-      }}
-      wrapperCol={{
-        span: 24,
-      }}
-      initialValues={
-        { isMarried: false }
-      }
-      style={{
-        maxWidth: 600,
-      }}
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-    >
-      <Form.Item
-        label="FirstName"
-        name="firstname"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your FirstName!',
-          },
-        ]}
+  async function edit(id) {
+    setSelected(id)
+    showModal();
+    let { data } = await request.get(`teacher/${id}`);
+    form.setFieldsValue(data);
+  }
+
+  return (
+    <Fragment>
+      <Table bordered
+        title={() => (
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <h1>Teachers</h1>
+            <Button onClick={showModal} type='primary'>Add</Button>
+          </div>
+        )}
+        columns={columns} dataSource={data} />
+      <Modal
+        title="Teacher Modal"
+        visible={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText={selected === null ? 'Add teacher' : 'Save info'}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="LastName"
-        name="lasttname"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your LastName!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="image"
-        name="avatar"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your LastName!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="Is Married"
-        wrapperCol={{
-          span: 24,
-        }}
-        valuePropName="checked"
-      >
-        <Checkbox>Is Married</Checkbox>
-      </Form.Item>
-    </Form>
-  </Modal>
-</Fragment>
+        <Form
+          form={form}
+          name="basic"
+          labelCol={{
+            span: 24,
+          }}
+          wrapperCol={{
+            span: 24,
+          }}
+          initialValues={
+            { IsMarried: false }
+          }
+          style={{
+            maxWidth: 600,
+          }}
+        >
+          <Form.Item
+            label="FirstName"
+            name="firstName"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your FirstName!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="LastName"
+            name="lastName"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your LastName!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Image"
+            name="avatar"
+            rules={[
+              {
+                required: true,
+                message: 'Please input an Image URL!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="IsMarried"
+            wrapperCol={{
+              span: 24,
+            }}
+            valuePropName="checked"
+          >
+            <Checkbox>Is Married</Checkbox>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Fragment>
+  );
 };
 
 export default TeacherPage;
